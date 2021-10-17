14dfe25e3f6e87d3dcca81fbe1940ee095674c96
Felipe Morán <felipe.moran11@gmail.com>
2021-10-17 12:43:20 -0300

    Exercises submission

---
+++ src/components/PerspectiveDecorator/PerspectiveDecorator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './PerspectiveDecorator.module.css';
+
+const PerspectiveDecorator = () => {
+  return (
+    <div className={styles.perspective_decorator}>
+    </div>
+  );
+};
+
+export default PerspectiveDecorator;
